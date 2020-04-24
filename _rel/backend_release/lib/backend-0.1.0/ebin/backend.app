bf3291a4d01c20d886fc5035ddcc1540b53f825f
Ubuntu <ubuntu@ip-172-31-44-219.us-east-2.compute.internal>
2020-04-24 20:37:18 +0000

    Handlers get, create, update, delete

--- _rel/backend_release/lib/backend-0.1.0/ebin/backend.app
+++ _rel/backend_release/lib/backend-0.1.0/ebin/backend.app
@@ -1,7 +1,7 @@
 {application, 'backend', [
 	{description, "New project"},
 	{vsn, "0.1.0"},
-	{modules, ['backend_app','backend_sup','hello_handler']},
+	{modules, ['backend_app','backend_sup','hello_handler','toppage_h']},
 	{registered, [backend_sup]},
 	{applications, [kernel,stdlib,cowboy]},
 	{mod, {backend_app, []}},
--- src/backend_app.erl
+++ src/backend_app.erl
@@ -6,12 +6,20 @@
 
 start(_Type, _Args) ->
 	Dispatch = cowboy_router:compile([
-        	{'_', [{"/", hello_handler, []}]}
-    	]),
-    	{ok, _} = cowboy:start_clear(my_http_listener,
+        {'_', [
+               {"/list", toppage_h, [list]},
+               {"/get/:record_id", toppage_h, [get]},
+               {"/create", toppage_h, [create]},
+               {"/update/:record_id", toppage_h, [update]},
+               {"/delete/:record_id", toppage_h, [delete]},
+               {"/help", toppage_h, [help]},
+               {"/", toppage_h, [help]}
+              ]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener,
         	[{port, 80}],
         	#{env => #{dispatch => Dispatch}}
-    	),	
+    ),
 	backend_sup:start_link().
 
 stop(_State) ->
